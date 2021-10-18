a151d4728e5c7610d60119ab42790bfe02978d7b
Magnus Kjellberg <magnus.kjellberg@istone.com>
2021-10-18 12:31:51 +0200

    Added example call to fetch from the API.

--- src/lib/api/inriver.ts
+++ src/lib/api/inriver.ts
@@ -25,3 +25,21 @@ const inriverFetch = async (
 
   return fetch(baseUrl + url, reqInit);
 };
+
+// example call to fetch entity summary.
+export const getEntitySummary = async (
+  entityId: string
+): Promise<EntitySummary> => {
+  //https://apieuw.productmarketingcloud.com/api/v1.0.0/entities/4337/summary
+  const entitySummaryResponse = await inriverFetch(
+    `entities/${entityId}/summary`
+  );
+  const entitySummary = (await entitySummaryResponse.json()) as EntitySummary;
+  return entitySummary;
+};
+
+// simple interface with just the fields id and fieldSetId from the response.
+export interface EntitySummary {
+  id: number;
+  fieldSetId: string;
+}
